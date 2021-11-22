5265dbd5d78967d754963e830eca4fa3515cd3e3
User <zhangir003@mail.ru>
2021-11-22 15:17:45 +0600

    `find_unique`

--- test.php
+++ test.php
@@ -1,15 +1,22 @@
 <?php
-function upend($text)
-{
-    return join('', array_reverse(preg_split('//u', $text, -1, PREG_SPLIT_NO_EMPTY)));
+function give_big_size(){
+    return rand(1000,2000);
 }
-
-function replace_words($sentence){
-$array = explode(' ',$sentence );
-$str = "";
-foreach($array as $item){
-    $str .= upend((string)$item)." ";
+function get_array(){
+    $arr = array();
+    for($i = 0; $i < give_big_size();$i++){
+        $arr[] = rand();
+    }
+    return $arr;
 }
-echo $str;
-};
-replace_words("HELLO WORDS IN");
+function find_individual($arr1,$arr2){
+    $list_individual = array();
+    for($i = 0; $i < count($arr1);$i++){
+        for($j = 0; $j < count($arr2) ;$j++){
+            if($arr1[$i] != $arr2[$j]){
+                $list_individual[] = $arr1[$i];
+            }
+        }
+    }
+    return $list_individual;
+}
