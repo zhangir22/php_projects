1870194909781804a624f29c71c596c91e3570a3
User <zhangir003@mail.ru>
2021-11-22 15:20:10 +0600

    `find_unique`

--- test.php
+++ test.php
@@ -5,18 +5,19 @@ function give_big_size(){
 function get_array(){
     $arr = array();
     for($i = 0; $i < give_big_size();$i++){
-        $arr[] = rand();
+        $arr[] = rand(1000,2000);
     }
     return $arr;
 }
 function find_individual($arr1,$arr2){
-    $list_individual = array();
-    for($i = 0; $i < count($arr1);$i++){
-        for($j = 0; $j < count($arr2) ;$j++){
-            if($arr1[$i] != $arr2[$j]){
-                $list_individual[] = $arr1[$i];
-            }
-        }
-    }
-    return $list_individual;
+    return array_diff($arr1,$arr2);
 }
+function show_array($arr){
+    foreach($arr as $item){
+        echo $item."<br>";
+    }
+}
+$arr1 = get_array();
+$arr2 = get_array();
+$arr3 = find_individual($arr1,$arr2);
+echo count($arr3).' '.count($arr1).' '.count($arr2);
